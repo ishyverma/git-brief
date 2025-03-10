65cbf0d035c5507a54257ba9a6bb9735d3053099
ishyverma <ishyamver@gmail.com>
2025-03-10 10:41:28 +0530

    added chatbot and ended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
 export default async function Home() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in")
+  }
   redirect("/dashboard")
   return <div>
     GIT BRIEF
